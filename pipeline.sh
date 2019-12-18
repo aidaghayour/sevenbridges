ea5556987b33338f9a45a9e2fc7e20e8bd4b7d1c
aidaghayour <aghayourkhia@mun.ca>
2019-12-18 02:35:46 +0000

    bashs

---
+++ pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Download genome graph file and locate it in current direcoty
+
+
+
+
+#Download the decoy genome reference
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/human_g1k_v37_decoy.fasta.gz
+gunzip human_g1k_v37_decoy.fasta.gz
+
+
+#install Docker
