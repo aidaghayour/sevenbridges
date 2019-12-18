34724d25dca75e9e63b41aa1df5a4e939069f8c2
Aida Ghayour <aghayourkhia@mun.ca>
2019-12-18 14:13:16 -0330

    samtools added

--- pipeline.sh
+++ pipeline.sh
@@ -25,3 +25,8 @@ sudo docker run -ti -v ~:/mountedcwd 38bda2551165
 cd ../..
 ################### READ MAPPING: needs reference FASTA, VCF graph (.vcf.gz!), two reads that are complimentary. The result .BAM file named sample1 will be saved in current repository.
 ./usr/local/bin/aligner --vcf ./mountedcwd/SBG.Graph.B37.V6.rc6.vcf.gz --reference ./mountedcwd/human_g1k_v37_decoy.fasta -q ./mountedcwd/example_human_Illumina.pe_1.fastq -Q ./mountedcwd/example_human_Illumina.pe_2.fastq -o ./mountedcwd/sample1.bam --read_group_sample 'SAMPLE_READ_GROUP' --read_group_library 'lib' --threads 4
+
+################### MAke sure samtools and all dependin libraries are installed
+################### Exit the container c+D
+samtools sort sample.bam > sample.sort.bam
+samtools index sample.sort.bam
